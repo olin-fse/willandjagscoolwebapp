c83504d9557768e6d87f51b6345348a7d1affff7
Jeremy Garcia <jeremiah.garcia@students.olin.edu>
2018-02-21 19:25:16 -0500

    add componentdidmount

--- frontend/containers/ToDo.jsx
+++ frontend/containers/ToDo.jsx
@@ -13,17 +13,16 @@ class ToDo extends React.Component {
     todos: ['Click to remove', 'Learn React', 'Write Code', 'Ship App'],
   }
 
-  // componentdidmount(){
-  //   fetch('/showTodos', {
-  //     method: 'GET',
-  //     headers: {
-  //       "Content-type": "application/json; charset=UTF-8"
-  //     },
-  //   })
-  //     .then((response) => { return response.json(); })
-  //     //.then((json) => {        })
-  //     });
-  // }
+  componentDidMount(){
+    fetch('/showTodos', {
+      method: 'GET',
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+    })
+      .then((response) => { return response.json(); })
+      .then((json) => { console.log(json) });
+  }
 
   onAddTodo = (text) => {
     const {todos} = this.state
